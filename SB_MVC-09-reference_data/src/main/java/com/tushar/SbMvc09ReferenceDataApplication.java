fb764d256882a5716c38ec472797b47955b13261
Tushar <Tushar@Tushar.mshome.net>
2021-10-13 21:57:21 +0530

    Reference Data: Radio button, CheckBox, Dropdown select box

---
+++ SB_MVC-09-reference_data/src/main/java/com/tushar/SbMvc09ReferenceDataApplication.java
@@ -0,0 +1,13 @@
+package com.tushar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMvc09ReferenceDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMvc09ReferenceDataApplication.class, args);
+	}
+
+}
---
+++ SB_MVC-09-reference_data/src/main/java/com/tushar/entity/Student.java
@@ -0,0 +1,81 @@
+package com.tushar.entity;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Arrays;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name="REFDATA_STUDENT_DETAILS")
+@SQLDelete(sql = "UPDATE  REFDATA_STUDENT_DETAILS  SET ISDELETED='Y' WHERE ENROLLID=?")
+@Where(clause = "ISDELETED <> 'Y' ")
+public class Student implements Serializable {
+
+	@Id
+	@SequenceGenerator(name = "ENRLOLL_GEN", sequenceName = "ROLL_SEQ_REFDATA",initialValue = 1, allocationSize = 1)
+	@GeneratedValue(generator="ENRLOLL_GEN", strategy =GenerationType.SEQUENCE)
+	private Integer ENROLLID;
+	
+	private Integer ROLLID;
+	
+	private String fname;
+	
+	private String mname;
+	
+	private String lname;
+	
+	private String address1;
+	
+	private String address2;
+	
+	private String currentStandard;
+	
+	private String mobile;
+
+	private String dob;
+
+	private String email;
+
+	private String passwrd;
+	
+	private String gender;
+	
+	private String ISDELETED="N";
+	
+	@Transient
+	private String vflag="N";
+	
+	private String newsletter;
+	
+	private String terms;
+
+	@Override
+	public String toString() {
+		return "Student [ENROLLID=" + ENROLLID + ", ROLLID=" + ROLLID + ", fname=" + fname + ", mname=" + mname
+				+ ", lname=" + lname + ", address1=" + address1 + ", address2=" + address2 + ", currentStandard="
+				+ currentStandard + ", mobile=" + mobile + ", dob=" + dob + ", email=" + email + ", passwrd=" + passwrd
+				+ ", gender=" + gender + ", ISDELETED=" + ISDELETED + ", vflag=" + vflag + ", newsletter=" + newsletter
+				+ ", terms=" + terms + "]";
+	}
+
+	
+}
+
