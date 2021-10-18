8d2b9536252a647a0c5c65e64800192ba66ac2a1
Tushar <Tushar@Tushar.mshome.net>
2021-10-18 21:56:05 +0530

    Xml View Resolver

---
+++ SB_MVC-18-Xml_ViewResolver/src/main/java/com/tushar/Config/CustomMvcConfigurer.java
@@ -0,0 +1,49 @@
+package com.tushar.Config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+import com.tushar.Interceptors.EOD_TimeCheck_Interceptor;
+
+@Component
+public class CustomMvcConfigurer implements WebMvcConfigurer {
+
+	@Autowired
+	private LocaleChangeInterceptor lci;
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		System.out.println("CustomMvcConfigurer.addInterceptors()");
+		registry.addInterceptor(new EOD_TimeCheck_Interceptor());
+		registry.addInterceptor(lci);
+	}
+	
+	/*	//For InternalResourceViewResolver: Enable this if main class method is commented
+		@Override
+		public void configureViewResolvers(ViewResolverRegistry registry) {
+			System.out.println("CustomMvcConfigurer.configureViewResolvers(): InternalResourceViewResolver");
+			InternalResourceViewResolver irvResolver  = new InternalResourceViewResolver();
+			irvResolver.setPrefix("/WEB-INF/JSP_Pages/");
+			irvResolver.setSuffix(".jsp");
+			registry.viewResolver(irvResolver);
+		}*/
+	
+	/*//For UrlBasedViewResolver
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		System.out.println("CustomMvcConfigurer.configureViewResolvers(): UrlBasedViewResolver");
+		UrlBasedViewResolver urlResolver  = new UrlBasedViewResolver();
+		urlResolver.setPrefix("/WEB-INF/JSP_Pages/");
+		urlResolver.setSuffix(".jsp");
+		urlResolver.setViewClass(JstlView.class);
+		registry.viewResolver(urlResolver);
+	}*/
+		
+}
---
+++ SB_MVC-18-Xml_ViewResolver/src/main/java/com/tushar/SbMvcApplication.java
@@ -0,0 +1,83 @@
+package com.tushar;
+
+import java.util.Locale;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.view.XmlViewResolver;
+
+@SpringBootApplication
+public class SbMvcApplication {
+
+	
+	/*//for InternalResourceViewResolver: Disable this if CustomMvcConfigurer class method is written
+	@Bean
+	public InternalResourceViewResolver configureViewResolver() {
+		System.out.println("SbMvcApplication.configureViewResolver():InternalResourceViewResolver");
+		InternalResourceViewResolver irvResolver  = new InternalResourceViewResolver();
+		irvResolver.setPrefix("/WEB-INF/JSP_Pages/");
+		irvResolver.setSuffix(".jsp");
+		return irvResolver;
+	}*/
+	
+	/*//for ResourceBundleViewResolver
+	 * @Bean
+	public ResourceBundleViewResolver configureViewResolver() {
+		System.out.println("SbMvcApplication.configureViewResolver(): ResourceBundleViewResolver");
+		ResourceBundleViewResolver  rbvs = new ResourceBundleViewResolver();
+		rbvs.setBasename("com/tushar/common/views");
+		return rbvs;
+	}*/
+	
+	 @Bean
+		public XmlViewResolver configureViewResolver() {
+			System.out.println("SbMvcApplication.configureViewResolver(): XmlViewResolver");
+			XmlViewResolver  xvResolver = new XmlViewResolver();
+			//reads to default /WEB-INF/views.xml
+			return xvResolver;
+	 }
+
+	@Bean("multiPartResolver")
+	public CommonsMultipartResolver createMultipartResolver () {
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+		multipartResolver.setMaxUploadSizePerFile(-1);
+		multipartResolver.setMaxUploadSize(-1);
+		multipartResolver.setPreserveFilename(true);
+		return multipartResolver;
+	}
+
+	@Bean("messageSource")//fixed id
+	public ResourceBundleMessageSource createLocaleSpecificText() {
+		ResourceBundleMessageSource rbms  =new ResourceBundleMessageSource();
+		rbms.setBasenames("com/tushar/common/App");
+		return rbms;
+	}
+	
+	@Bean("localeResolver")//fixed id
+	public SessionLocaleResolver setDefaultLocale() {
+		SessionLocaleResolver locale = new SessionLocaleResolver();
+		locale.setDefaultLocale(new Locale("en", "US"));
+		return locale;
+	}
+	
+	@Bean("lci")// not fixed id
+	public LocaleChangeInterceptor handleLocaleRequest() {
+		LocaleChangeInterceptor localeIcp = new LocaleChangeInterceptor();
+		localeIcp.setParamName("lang");
+		return localeIcp;
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SbMvcApplication.class, args);
+	}
+
+}
