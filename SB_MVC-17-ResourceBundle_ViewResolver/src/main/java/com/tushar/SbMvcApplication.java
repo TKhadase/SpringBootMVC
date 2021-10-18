019e874681b68b566a00a9db303d9e1fc1042e2e
Tushar <Tushar@Tushar.mshome.net>
2021-10-18 10:18:24 +0530

    ResourceBundle ViewResolver

---
+++ SB_MVC-17-ResourceBundle_ViewResolver/src/main/java/com/tushar/SbMvcApplication.java
@@ -0,0 +1,71 @@
+package com.tushar;
+
+import java.util.Locale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.ResourceBundleViewResolver;
+
+@SpringBootApplication
+public class SbMvcApplication {
+
+	/*//for InternalResourceViewResolver: Disable this if CustomMvcConfigurer class method is written
+	@Bean
+	public InternalResourceViewResolver configureInternalResourceViewResolver() {
+		System.out.println("SbMvcApplication.configureInternalResourceViewResolver()");
+		InternalResourceViewResolver irvResolver  = new InternalResourceViewResolver();
+		irvResolver.setPrefix("/WEB-INF/JSP_Pages/");
+		irvResolver.setSuffix(".jsp");
+		return irvResolver;
+	}*/
+	
+	@Bean
+	public ResourceBundleViewResolver configureResourceBundleViewResolver() {
+		System.out.println("SbMvcApplication.configureResourceBundleViewResolver(): ResourceBundleViewResolver");
+		ResourceBundleViewResolver  rbvs = new ResourceBundleViewResolver();
+		rbvs.setBasename("com/tushar/common/views");
+		return rbvs;
+	}
+
+	@Bean("multiPartResolver")
+	public CommonsMultipartResolver createMultipartResolver () {
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+		multipartResolver.setMaxUploadSizePerFile(-1);
+		multipartResolver.setMaxUploadSize(-1);
+		multipartResolver.setPreserveFilename(true);
+		return multipartResolver;
+	}
+
+	@Bean("messageSource")//fixed id
+	public ResourceBundleMessageSource createLocaleSpecificText() {
+		ResourceBundleMessageSource rbms  =new ResourceBundleMessageSource();
+		rbms.setBasenames("com/tushar/common/App");
+		return rbms;
+	}
+	
+	@Bean("localeResolver")//fixed id
+	public SessionLocaleResolver setDefaultLocale() {
+		SessionLocaleResolver locale = new SessionLocaleResolver();
+		locale.setDefaultLocale(new Locale("en", "US"));
+		return locale;
+	}
+	
+	@Bean("lci")// not fixed id
+	public LocaleChangeInterceptor handleLocaleRequest() {
+		LocaleChangeInterceptor localeIcp = new LocaleChangeInterceptor();
+		localeIcp.setParamName("lang");
+		return localeIcp;
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SbMvcApplication.class, args);
+	}
+
+}
